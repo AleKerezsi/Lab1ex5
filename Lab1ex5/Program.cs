23f0f39395d1b4e7545c64c780c22d1865fff3b0
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 16:34:46 +0200

    Add project files.

---
+++ Lab1ex5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab1ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare */
+
+            Console.WriteLine("Introduceti numarul x: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduceti numarul y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine(y + " " + x);
+            }
+
+            if (y > x)
+            {
+                Console.WriteLine(x + " " + y);
+            }
+
+            if (x == y)
+            {
+                Console.WriteLine("Numerele sunt identice");
+            }
+        
+        }
+    }
+}
